013f82e186ce75079ff8fdf84ffb907463cefa0f
Lucas Leal <lucas.silva@ebanx.com>
2019-02-20 14:44:44 -0300

    Handler test initial

--- tests/HandlerTest.php
+++ tests/HandlerTest.php
@@ -1,14 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lucas.silva
- * Date: 2019-02-20
- * Time: 13:15
- */
+require_once __DIR__ . '/../server/Handler.php';
 
-class HelloTest extends \PHPUnit\Framework\TestCase
+class HandlerTest extends \PHPUnit\Framework\TestCase
 {
-    public function testHello(): void {
-        $this->assertEquals(true, 1==1);
+    public function testValidateBoletoFields(): void {
+        $_POST['value'] = 10.50;
+        $_POST['name'] = 'Carlos de Oliveira';
+        $_POST['document'] = '062.965.419-02';
+        $_POST['email'] = 'carlos.oliveira@ebanx.com';
+        $_POST['phone-number'] = '41999998765';
+        $_POST['zipcode'] = '82510180';
+        $_POST['state'] = 'Paraná';
+        $_POST['city'] = 'Curitiba';
+        $_POST['street'] = 'Rua Marechal Deodoro';
+        $_POST['address-number'] = '650';
+        $_POST['payment-type'] = 'boleto';
+
+        $handler = new Handler($_POST);
+        $this->assertTrue($handler->validateFields());
+    }
+
+    public function testValidateBoletoFieldsShouldFail(): void {
+        $_POST['value'] = 10.50;
+        $_POST['name'] = '';
+        $_POST['document'] = '062.965.419-02';
+        $_POST['email'] = 'carlos.oliveira@ebanx.com';
+        $_POST['phone-number'] = '41999998765';
+        $_POST['zipcode'] = '82510180';
+        $_POST['state'] = 'Paraná';
+        $_POST['city'] = 'Curitiba';
+        $_POST['street'] = 'Rua Marechal Deodoro';
+        $_POST['address-number'] = '650';
+        $_POST['payment-type'] = 'boleto';
+
+        $handler = new Handler($_POST);
+        $this->assertFalse($handler->validateFields());
     }
 }
