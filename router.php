3394af8c80d89e2f682eab6b9ec91791a90de6f6
Ana Magnoni <ana.magnoni@ebanx.com>
2019-02-20 11:26:01 -0300

    router.php function added

---
+++ router.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ana.magnoni
+ * Date: 2019-02-20
+ * Time: 11:02
+ */
+
+if (preg_match('/payment/', $_SERVER["REQUEST_URI"])) {
+    echo "<p> Request payment </p>";
+} else {
+    header("Location: public/index.html");
+}
+?>
